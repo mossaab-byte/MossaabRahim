e01556cb1cadcd34c7ae35c9a328b0125250e9f8
mossaab-byte <mossaabrahim2@gmail.com>
2026-01-13 18:52:28 +0100

    finally

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -1,11 +1,16 @@
+"""
+WSGI config for backend project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
+"""
+
 import os
-import sys
-
-path = '/home/mossaab-byte/MossaabRahim'
-if path not in sys.path:
-    sys.path.append(path)
-
-os.environ['DJANGO_SETTINGS_MODULE'] = 'backend.settings'
 
 from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
+
 application = get_wsgi_application()
